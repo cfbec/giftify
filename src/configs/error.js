1d24c925d33648e8d55feca412c75eae5ffe67a8
Cristian Becerra <c.becerra.valdes@gmail.com>
2020-10-04 09:51:47 -0300

    Fixed comment error middleware

--- src/configs/error.js
+++ src/configs/error.js
@@ -4,7 +4,7 @@ module.exports = () => async (ctx, next) => {
   } catch (err) {
     console.error(err);
     if (err.status || err.name === 'MongoError') {
-      // MongoError handle others code ie. E11000 duplicate key. 
+      // MongoError handle others code, for example E11000 duplicate key (schema constraints). 
       // For all these types of error only conflict error (409) is indicated
       ctx.response.status = err.status || 409;
       ctx.response.body = {
